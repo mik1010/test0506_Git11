c36a79c1d6747f47dfee819983820ff7aeeebbc1
Mike <mmm@gmail.com>
2019-05-06 17:31:17 +0800

    ccc

--- test0506_Git11/Form1.cs
+++ test0506_Git11/Form1.cs
@@ -16,5 +16,10 @@ namespace test0506_Git11
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
